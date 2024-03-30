5191e231ff282f4ae0b68b637c9b913efdb3d84e
Philz7789 <philz1mir4@gmail.com>
2024-03-30 15:19:28 +0700

    homework1
    section 5 exercise 4

---
+++ Exercise4_Padchayapong_K.py
@@ -0,0 +1,13 @@
+print("-----Your Final Score-----")
+
+FEN = input()
+GBU = input()
+ICS = input()
+CP = input()
+
+print("คุณได้คะแนนวิชา ","Foundation English ","= ",(FEN))
+print("คุณได้คะแนนวิชา ","General Business ","= ",(GBU))
+print("คุณได้คะแนนวิชา ","Introduction to Computer Systems ","= ",(ICS))
+print("คุณได้คะแนนวิชา ","Computer Programming ","= ",(CP))
+
+print("ยินดีด้วยสำหรับคะแนนสอบ")
